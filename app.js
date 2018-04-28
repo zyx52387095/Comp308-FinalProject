e5d39f7e41c570d12a7339896376d480e207b02a
Will Zhang <zephyr@ZephyrdeMacBook-Pro.local>
2018-04-28 11:19:39 -0400

    seed planted

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//todo
+
+const express = require('express');
+const app = express();
+
+const appRoutes = require('./routes/app');
+
+//adding express middleware to serve static pages - e.g style.css
+app.use("/public", express.static(__dirname+'/public'));
+
+//set view engine to be ejs engine
+app.set('view engine', 'ejs');
+
+
+app.use('/', appRoutes);
+
+app.listen(3000);
+console.log("app started on port ", 3000);
